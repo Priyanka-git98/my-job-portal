80f68dd1b1efb80e4fd2ea2bc123365b62115bf5
Priyanka-git98 <148323876+Priyanka-git98@users.noreply.github.com>
2023-11-30 16:20:28 +0530

    header initiated

---
+++ src/app/components/Header.js
@@ -0,0 +1,32 @@
+// Header.js
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Link from 'next/link';
+
+const Header = () => {
+  return (
+    <AppBar position="static" style={{ background: '#2CE2A2' }}>
+      <Toolbar>
+        <Typography variant="h6" component="div">
+          <Link href="/" legacyBehavior>
+            <a style={{ color: 'black', textDecoration: 'none' }}>
+             Hirect
+            </a>
+          </Link>
+        </Typography>
+        <div style={{ marginLeft: 'auto' }}>
+          <Link href="/findjobs" legacyBehavior>
+            <a style={{ color: 'black', marginLeft: 20, textDecoration:"none" }}>Find Jobs</a>
+          </Link>
+          <Link href="/postjobs" legacyBehavior>
+            <a style={{ color: 'black', marginLeft: 20, textDecoration:"none"}}>Employers/Post Job</a>
+          </Link>
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
